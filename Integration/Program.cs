6de49b156450a0ec71b2892dbef36d96f92ea910
Buğra Han Sönmez <bugra.sonmez@ayssoft.com>
2024-06-11 17:43:58 +0300

    test case

---
+++ Integration/Program.cs
@@ -0,0 +1,24 @@
+﻿using Integration.Service;
+
+namespace Integration;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        var service = new ItemIntegrationService();
+        
+        ThreadPool.QueueUserWorkItem(_ => Console.WriteLine(service.SaveItem("a").Message));
+        ThreadPool.QueueUserWorkItem(_ => Console.WriteLine(service.SaveItem("b").Message));
+        ThreadPool.QueueUserWorkItem(_ => Console.WriteLine(service.SaveItem("c").Message));
+        Thread.Sleep(500);
+
+        ThreadPool.QueueUserWorkItem(_ => Console.WriteLine(service.SaveItem("a").Message));
+        ThreadPool.QueueUserWorkItem(_ => Console.WriteLine(service.SaveItem("b").Message));
+        ThreadPool.QueueUserWorkItem(_ => Console.WriteLine(service.SaveItem("c").Message));
+
+        Thread.Sleep(5000);
+
+        Console.ReadLine();
+    }
+}
---
+++ Integration/Service/ItemIntegrationService.cs
@@ -0,0 +1,35 @@
+﻿using Integration.Common;
+using Integration.Backend;
+
+namespace Integration.Service;
+
+public sealed class ItemIntegrationService
+{
+    //This is a dependency that is normally fulfilled externally.
+    private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
+    private static object _lock = new object();
+    // This is called externally and can be called multithreaded, in parallel.
+    // More than one item with the same content should not be saved. However,
+    // calling this with different contents at the same time is OK, and should
+    // be allowed for performance reasons.
+    public Result SaveItem(string itemContent)
+    {
+        lock (_lock)
+        {
+            // Check the backend to see if the content is already saved.
+            if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+            {
+                return new Result(false, $"Duplicate item received with content {itemContent}.");
+            }
+
+            var item = ItemIntegrationBackend.SaveItem(itemContent);
+
+            return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+        }
+    }
+
+    public List<Item> GetAllItems()
+    {
+        return ItemIntegrationBackend.GetAllItems();
+    }
+}
